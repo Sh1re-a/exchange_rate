1b6a269743871cf7f9ba58eb191cb867b6b0ec52
sh1re-a <Shirwac.abib@gmail.com>
2026-03-20 13:33:21 +0100

    refactor: remove sout and add logger instead

--- src/main/java/se/salt/foreignexchangeapi/service/RateCacheService.java
+++ src/main/java/se/salt/foreignexchangeapi/service/RateCacheService.java
@@ -1,5 +1,7 @@
 package se.salt.foreignexchangeapi.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import se.salt.foreignexchangeapi.client.ApiClient;
@@ -8,12 +10,14 @@ import se.salt.foreignexchangeapi.dto.FrankfurterConversionResponse;
 import java.util.Map;
 import java.util.Optional;
 
+
 @Service
 public class RateCacheService {
 
     private final ApiClient apiClient;
     private int countRate = 0;
     private int countCurrencies = 0;
+    private final static Logger logger = LoggerFactory.getLogger(RateCacheService.class);
 
     public RateCacheService(ApiClient apiClient) {
         this.apiClient = apiClient;
@@ -25,14 +29,14 @@ public class RateCacheService {
         FrankfurterConversionResponse response =
                 apiClient.getRatesFromWantedCurrency(baseCurrency, wantedCurrency);
         countRate++;
-        System.out.println("Calling Frankfurter API getRate() " + countRate);
+        logger.info("Calling External Frankfurter API getRate() count={}", countRate);
         return Optional.ofNullable(response.rates().get(wantedCurrency))
                 .orElseThrow(() -> new IllegalStateException("Could not fetch rate from API"));
     }
     @Cacheable("currencies")
     public Map<String, String> getCurrencies(){
         countCurrencies++;
-        System.out.println("Calling Frankfurter API getCurrencies() " + countCurrencies);
+        logger.info("Calling External Frankfurter API getCurrencies() count={}", countCurrencies);
         return Optional.ofNullable(apiClient.getAllCurrenciesAndTheirName())
                 .orElseThrow(()  -> new IllegalStateException("Could not fetch currencies from API"));
     }
