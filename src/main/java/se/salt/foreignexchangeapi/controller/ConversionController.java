5a417353bc209b7cdb3f30a8972643e353dd0dfa
sh1re-a <Shirwac.abib@gmail.com>
2026-03-20 13:30:22 +0100

    feat: add logger till controller

--- src/main/java/se/salt/foreignexchangeapi/controller/ConversionController.java
+++ src/main/java/se/salt/foreignexchangeapi/controller/ConversionController.java
@@ -1,6 +1,8 @@
 package se.salt.foreignexchangeapi.controller;
 
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import se.salt.foreignexchangeapi.client.ApiClient;
@@ -15,6 +17,7 @@ import java.util.*;
 @RequestMapping("/api")
 public class ConversionController {
     private final ConversionService conversionService;
+    private static final Logger logger = LoggerFactory.getLogger(ConversionController.class);
 
 
     public ConversionController(ConversionService conversionService) {
@@ -37,19 +40,16 @@ public class ConversionController {
             @RequestHeader(value = "User-Agent", required = false)
             String userAgent){
 
-        return ResponseEntity.ok()
-                .header("User-Agent" , "Request from " +userAgent)
-                .body(conversionService.convert(
-                request.from(),
-                request.to(),
-                request.amount())
-        );
+        logger.info("Incoming conversion request User-Agent={} body: baseCurrency={}, targetCurrency={}, amount={}"
+                ,userAgent, request.from(),request.to(), request.amount());
+        ConversionResponse response = conversionService.convert(request.from(),request.to(),request.amount());
+        return ResponseEntity.ok().body(response);
     }
 
     @GetMapping("/currencies")
     public ResponseEntity<Map<String,String>> getCurrencies(@RequestHeader(value = "User-Agent", required = false) String userAgent) {
-        return ResponseEntity.ok()
-                .header("User-Agent", "Request from " + userAgent)
-                .body(conversionService.getAllCurrencies());
+        logger.info("Incoming get all currencies User-Agent={}", userAgent);
+        Map<String, String> currencies = conversionService.getAllCurrencies();
+        return ResponseEntity.ok().body(currencies);
     }
 }
