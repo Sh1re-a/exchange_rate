53d7c481d2c1b8a0db12a5fa44cff862b6cd8b7b
sh1re-a <Shirwac.abib@gmail.com>
2026-03-20 14:16:35 +0100

    refactor RequestLoggingFilter to use logger instead

--- src/main/java/se/salt/foreignexchangeapi/filter/RequestLoggingFilter.java
+++ src/main/java/se/salt/foreignexchangeapi/filter/RequestLoggingFilter.java
@@ -4,11 +4,15 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 public class RequestLoggingFilter extends OncePerRequestFilter {
+
+    private final static Logger logger = LoggerFactory.getLogger(RequestLoggingFilter.class);
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
@@ -17,14 +21,17 @@ public class RequestLoggingFilter extends OncePerRequestFilter {
         String path = request.getRequestURI();
         String userAgent = request.getHeader("User-Agent");
 
-        System.out.println("Incoming request: " + method + " " + path + " | User-Agent: " + userAgent);
+        logger.info("Incoming request={} path={} | User-Agent={}", method , path, userAgent);
 
         filterChain.doFilter(request, response);
 
         long duration = System.currentTimeMillis() - start;
 
-        System.out.println("Completed request: " + method + " " + path +
-                " | Status: " + response.getStatus() +
-                " | Duration: " + duration + " ms");
+        logger.info("Completed request -> method={} path={} status={} durationMs={}",
+                method,
+                path,
+                response.getStatus(),
+                duration
+        );
     }
 }
